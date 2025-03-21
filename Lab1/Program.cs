95797d32b7be4bd99cbdd05f27cc7e845febe474
JadenBelle <jaden.belle@icloud.com>
2025-03-21 10:52:33 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -3,8 +3,21 @@
 class Program
 {
     static void Main(string[] args)
-    {
+    { 
+        //Step 1
+        Console.WriteLine("Jaden Belle IT-1050");
         Console.WriteLine("Hello, World!");
+      
+       /*Step 2 Jaden Belle
+         IT-1050
+       */
+      //Step 3 
+      int favoritenumber = 7; 
+      string favoriteprogramminglanguage = "Python";
+      double programswrittenbefore  =0;
+      bool hasprogrammingexperience = true;
+      
     }
 }
 Console.Writeline("Welcome to IT-1050!");
+
